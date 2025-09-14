dd4d6e294b3ef45700c10f4ab83e4806321d05e5
thurzyn001 <zynthur@gmail.com>
2025-09-14 17:53:03 -0300

    Move Hello World example to olamundo package
    Deleted ex001.java and added OlaMundo.java under the olamundo package with a localized greeting message.

--- ex001.java
+++
@@ -1,5 +0,0 @@
-public class ex001 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
---
+++ olamundo/OlaMundo.java
@@ -0,0 +1,7 @@
+package olamundo;
+
+public class OlaMundo{
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+}
