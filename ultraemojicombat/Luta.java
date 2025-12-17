77d62911b022e367e7e1aeb6cd198e5a1d91538a
Arthur Furtado <zynthur@gmail.com>
2025-12-17 19:09:48 -0300

    Mudei a as mensagens de erro para marcarLuta()
    Se a luta nao for aprovada no marcar luta, o lutar só fecha o quadrado e nao verifica qual foi o motivo, quem faz isso agora é o próprio marcarLuta()

--- ultraemojicombat/Luta.java
+++ ultraemojicombat/Luta.java
@@ -78,61 +78,66 @@ public class Luta implements InterfaceLuta {
             this.setAprovada(false);
             this.setDesafiado(l1);
             this.setDesafiante(l2);
+
+            if (getDesafiado() == getDesafiante()) {
+
+                System.out.println("Luta não aprovada: o desafiado e o desafiante são o mesmo lutador.");
+                System.out.println(getDesafiado().getNome() + " vs " + getDesafiante().getNome());
+
+            } else if (getDesafiado().getCategoria().equals("Inválido")) {
+
+                System.out.println("Luta não aprovada: o lutador " + getDesafiado().getNome() + 
+                " Está atualmente com " + getDesafiado().getPeso() + " Kgs" + " e não pode lutar,");
+
+                if (getDesafiado().getPeso() < 52.2){
+
+                    System.out.println("pois não atingiu o peso mínimo para a categoria de Peso Leve de 52.2 Kgs.");
+
+                } else if (getDesafiado().getPeso() > 120.2) {
+
+                    System.out.println("pois ultrapssou o peso máximo para a categoria de Peso Pesado de 120.2 Kgs");
+
+                }
+
+            } else if (getDesafiante().getCategoria().equals("Inválido")) {
+
+                System.out.println("Luta não aprovada: o lutador " + getDesafiante().getNome() + 
+                " Está atualmente com " + getDesafiante().getPeso() + " Kgs" + " e não pode lutar,");
+
+                if (getDesafiante().getPeso() < 52.2){
+
+                    System.out.println("pois não atingiu o peso mínimo para a categoria de Peso Leve de 52.2 Kgs.");
+
+                } else if (getDesafiante().getPeso() > 120.2) {
+
+                    System.out.println("pois ultrapssou o peso máximo para a categoria de Peso Pesado de 120.2 Kgs");
+
+                }
+
+            } else if (!getDesafiado().getCategoria().equals(getDesafiante().getCategoria())) {
+
+                System.out.println("Luta não aprovada: lutadores de categorias diferentes.");
+                System.out.println(getDesafiado().getNome() + " - " + getDesafiado().getCategoria() +
+                " vs " + getDesafiante().getNome() + " - " + getDesafiante().getCategoria());
+
+            }
+
         }
 
     }
 
+
     @Override
     public void luta() {
 
-        if (getDesafiado() == getDesafiante()) {
-
-            System.out.println("Luta não aprovada: o desafiado e o desafiante são o mesmo lutador.");
-            System.out.println(getDesafiado().getNome() + " vs " + getDesafiante().getNome());
-
-        } else if (getDesafiado().getCategoria().equals("Inválido")) {
-
-            System.out.println("Luta não aprovada: o lutador " + getDesafiado().getNome() + 
-            " Está atualmente com " + getDesafiado().getPeso() + " Kgs" + " e não pode lutar,");
-
-            if (getDesafiado().getPeso() < 52.2){
-
-                System.out.println("pois não atingiu o peso mínimo para a categoria de Peso Leve de 52.2 Kgs.");
-
-            } else if (getDesafiado().getPeso() > 120.2) {
-
-                System.out.println("pois ultrapssou o peso máximo para a categoria de Peso Pesado de 120.2 Kgs");
-            }
-
-        } else if (getDesafiante().getCategoria().equals("Inválido")) {
-
-            System.out.println("Luta não aprovada: o lutador " + getDesafiante().getNome() + 
-            " Está atualmente com " + getDesafiante().getPeso() + " Kgs" + " e não pode lutar,");
-
-            if (getDesafiante().getPeso() < 52.2){
-
-                System.out.println("pois não atingiu o peso mínimo para a categoria de Peso Leve de 52.2 Kgs.");
-
-            } else if (getDesafiante().getPeso() > 120.2) {
-
-                System.out.println("pois ultrapssou o peso máximo para a categoria de Peso Pesado de 120.2 Kgs");
-            }
-
-        } else if (!getDesafiado().getCategoria().equals(getDesafiante().getCategoria())) {
-
-            System.out.println("Luta não aprovada: lutadores de categorias diferentes.");
-            System.out.println(getDesafiado().getNome() + " - " + getDesafiado().getCategoria() +
-            " vs " + getDesafiante().getNome() + " - " + getDesafiante().getCategoria());
-            
-
-        } else {
+        if (this.getAprovada()) {
 
             getDesafiado().apresentar();
             getDesafiante().apresentar();
 
             System.out.println("");
 
-            int vencedor = (Math.random() * 3);
+            int vencedor = (int) (Math.random() * 3);
 
             switch (vencedor) {
                 
